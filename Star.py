13507b641a58a731f31df3583b361b77809db48f
NRHAMIM <lopafeardous@gmail.com>
2025-05-13 02:58:02 +0600

    Create Star.py

---
+++ Star.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.color("yellow")
+turtle.fillcolor("aqua")
+turtle.fillcolor("blue")
+turtle.bgcolor("black")
+
+turtle.width(12)
+
+for i in range(5):
+    turtle.forward(190)
+    turtle.right(144)
+
+turtle.done()
