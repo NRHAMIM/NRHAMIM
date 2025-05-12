ce24cedabd6e5f3661dd3d45c3bfc8feb4909064
NRHAMIM <lopafeardous@gmail.com>
2025-05-13 03:03:50 +0600

    Create Ei j hello.py
    Ei j ei j hello

---
+++ Ei j hello.py
@@ -0,0 +1,8 @@
+Name=input("Enter name:     ")
+if   Name.startswith("Mr.")or Name.startswith('Md'):
+         print("ei j hello sir ")
+         
+elif  Name.startswith("Mrs")or Name.startswith('Mis'):
+    print("ei j hello mam")
+else :
+    print("ei j hello,",Name)  
